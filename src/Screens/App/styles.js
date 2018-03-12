a212483bc3c6238ee9e069c4cba78b8048d31101
FrankIglesias <franciscoj.iglesias@hotmail.com>
2018-03-12 17:54:40 -0300

    Finished layout

---
+++ src/Screens/App/styles.js
@@ -0,0 +1,40 @@
+import { StyleSheet } from 'react-native'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'space-between'
+  },
+  header: {
+    height: 50,
+    marginBottom: 10,
+    backgroundColor: 'skyblue',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  headerText: {
+    color: 'white',
+    fontWeight: 'bold'
+  },
+  input: {
+    height: 50,
+    paddingVertical: 5,
+    borderWidth: 0,
+  },
+  removeButton: {
+    padding: 10,
+    alignItems: 'center'
+  },
+  removeText: {
+    color: 'red'
+  },
+  bodyContainer: {
+    flex: 1,
+    borderBottomColor: '#DDD',
+    borderTopColor: '#DDD',
+    borderTopWidth: 1,
+    borderBottomWidth: 1
+  }
+});
+
+export default styles;
